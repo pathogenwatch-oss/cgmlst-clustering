7c70df18c4ce19a3f768b13eabe762f7e6bdd0a0
Ben Taylor <ben.taylor@sanger.ac.uk>
2018-03-23 14:07:49 +0000

    Add some more tests

--- main_test.go
+++ main_test.go
@@ -6,6 +6,51 @@ import (
 	"testing"
 )
 
+func BenchmarkConcurrentArrayUpdate(b *testing.B) {
+	array := make([]int, b.N)
+	indexes := make(chan int)
+	go func() {
+		for i := 0; i < b.N; i++ {
+			indexes <- i
+		}
+		close(indexes)
+	}()
+	var wg sync.WaitGroup
+	for worker := 0; worker < 100; worker++ {
+		wg.Add(1)
+		go func() {
+			for {
+				if i, more := <-indexes; !more {
+					wg.Done()
+					return
+				} else {
+					array[i] = i * i
+				}
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func TestTokeniser(t *testing.T) {
+	tokens := NewTokeniser()
+	if token := tokens.Get(AlleleKey{"foo", 1}); token != 0 {
+		t.Fatal("Wanted 0")
+	}
+	if token := tokens.Get(AlleleKey{"foo", 1}); token != 0 {
+		t.Fatal("Wanted 0")
+	}
+	if token := tokens.Get(AlleleKey{"bar", 1}); token != 1 {
+		t.Fatal("Wanted 1")
+	}
+	if token := tokens.Get(AlleleKey{"foo", 1}); token != 0 {
+		t.Fatal("Wanted 0")
+	}
+	if token := tokens.Get(AlleleKey{"foo", "1"}); token != 2 {
+		t.Fatal("Wanted 2")
+	}
+}
+
 func BenchmarkScoreAll(b *testing.B) {
 	profiles, err := os.Open("../all_staph.bson")
 	if err != nil {
