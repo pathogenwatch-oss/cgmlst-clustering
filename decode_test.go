df1bca1ceb58ad60e14062f28d137e452ff707b9
Richard Goater <richard.goater27@gmail.com>
2018-04-04 16:33:44 +0100

    Trying custom parser

---
+++ decode_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCustomParse(t *testing.T) {
+	testFile, err := os.Open("testdata/genomes.bson")
+	if err != nil {
+		t.Fatal("Couldn't load test data")
+	}
+	errc := make(chan error)
+	c := readBsonDocs(testFile, errc)
+	doc := <-c
+	var p ProfileDoc2
+	if err := unmarshal(doc, &p); err != nil {
+		t.Fatal(err)
+	}
+	if p.FileID != "1b5be653a771dd81b5d9c290a6ac1c17f8999c97" {
+		t.Fatal("Wrong fileID")
+	}
+	if p.OrganismID != "1280" {
+		t.Fatal("Wrong organismId")
+	}
+	// t.Fatal(len(p.Analysis.Cgmlst.Matches))
+	if len(p.Analysis.Cgmlst.Matches) != 2226 {
+		t.Fatalf("Wrong number of matches %d", len(p.Analysis.Cgmlst.Matches))
+	}
+}
+
+func BenchmarkCustomParse(b *testing.B) {
+	testFile, err := os.Open("testdata/genomes.bson")
+	if err != nil {
+		b.Fatal("Couldn't load test data")
+	}
+	errc := make(chan error)
+	c := readBsonDocs(testFile, errc)
+	doc := <-c
+	var p ProfileDoc2
+	if err := unmarshal(doc, &p); err != nil {
+		b.Fatal(err)
+	}
+	if p.FileID != "1b5be653a771dd81b5d9c290a6ac1c17f8999c97" {
+		b.Fatal("Wrong fileId")
+	}
+	if p.OrganismID != "1280" {
+		b.Fatal("Wrong organismId")
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var p1 ProfileDoc2
+		if err := unmarshal(doc, &p1); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
--- parseInput_test.go
+++ parseInput_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"testing"
+
+	"github.com/pkg/bson"
 )
 
 // func TestParseGenomeDoc(t *testing.T) {
@@ -273,6 +275,73 @@ func TestNewScores(t *testing.T) {
 				t.Fatalf("Got %d, expected %d", calc, idx)
 			}
 			idx++
+		}
+	}
+}
+
+type _ProfileDoc struct {
+	Id         bson.ObjectId
+	Organismid string
+	Fileid     string
+	Public     bool
+	Analysis   struct {
+		Cgmlst struct {
+			Version string
+			Matches []struct {
+				Gene string
+				// Id   int
+			}
+		}
+	}
+}
+
+func TestStructParse(t *testing.T) {
+	testFile, err := os.Open("testdata/genomes.bson")
+	if err != nil {
+		t.Fatal("Couldn't load test data")
+	}
+	errc := make(chan error)
+	c := readBsonDocs(testFile, errc)
+	doc := <-c
+	var p _ProfileDoc
+	if err := bson.Unmarshal(doc, &p); err != nil {
+		t.Fatal(err)
+	}
+	if p.Fileid != "1b5be653a771dd81b5d9c290a6ac1c17f8999c97" {
+		t.Fatal("Wrong fileId")
+	}
+	if p.Organismid != "1280" {
+		t.Fatal("Wrong organismId")
+	}
+	t.Fatal(len(p.Analysis.Cgmlst.Matches))
+	// if len(p.Analysis.Cgmlst.Matches) != 2226 {
+	// 	t.Fatalf("Wrong number of matches %d", len(p.Analysis.Cgmlst.Matches))
+	// }
+}
+
+func BenchmarkStructParse(b *testing.B) {
+	testFile, err := os.Open("testdata/genomes.bson")
+	if err != nil {
+		b.Fatal("Couldn't load test data")
+	}
+	errc := make(chan error)
+	c := readBsonDocs(testFile, errc)
+	doc := <-c
+	var p _ProfileDoc
+	if err := bson.Unmarshal(doc, &p); err != nil {
+		b.Fatal(err)
+	}
+	if p.Fileid != "1b5be653a771dd81b5d9c290a6ac1c17f8999c97" {
+		b.Fatal("Wrong fileId")
+	}
+	if p.Organismid != "1280" {
+		b.Fatal("Wrong organismId")
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var p1 _ProfileDoc
+		if err := bson.Unmarshal(doc, &p1); err != nil {
+			b.Fatal(err)
 		}
 	}
 }
