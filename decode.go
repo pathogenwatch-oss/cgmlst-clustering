ff69f62762884e8bd692f0f5278e5c90208ea704
Richard Goater <richard.goater27@gmail.com>
2018-04-09 12:35:53 +0100

    wip

--- decode.go
+++ decode.go
@@ -2,192 +2,16 @@ package main
 
 import (
 	"bytes"
-	"errors"
-	"fmt"
+	"io"
+
+	"gitlab.com/cgps/bsonkit"
 )
 
 type ObjectID [12]byte
 
 func trimlast(s []byte) []byte { return s[:len(s)-1] }
 
-// reader is an iterator over a BSON document.
-type reader struct {
-	// source bson document, mutated during read.
-	bson []byte
-
-	// ename of the next element, ename[0] contains the
-	// type of the element, tf. the smallest ename slice
-	// is 2 bytes. (0x02, 0x00)
-	ename []byte
-
-	// bson element that has been parsed _but_not_decoded_. The type
-	// of the element is stored in ename[0]
-	element []byte
-
-	// last error, if any
-	err error
-}
-
-// Next advances the reader to the next element in BSON document.
-// The element is available via the Element method. It returns false
-// when the end of the document is reached, or an error occurs.
-// After Next() returns false, the Err method will return any error
-// that occured during walking the document.
-func (r *reader) Next() bool {
-	switch len(r.bson) {
-	case 0:
-		// we've read everything
-		return false
-	case 1:
-		// error, there must be at least 2 bytes remaining to be
-		// valid BSON
-		r.err = errors.New("corrupt BSON, only 1 byte remains")
-		return false
-	}
-	i := bytes.IndexByte(r.bson[1:], 0)
-	if i < 0 {
-		r.err = errors.New("corrupt BSON ename")
-		return false
-	}
-	i += 2
-	ename, rest := r.bson[:i], r.bson[i:]
-	var element []byte
-	switch typ := ename[0]; typ {
-	case 0x01:
-		// double
-		if len(rest) < 8 {
-			r.err = errors.New("corrupt BSON reading double")
-			return false
-		}
-		element, rest = rest[:8], rest[8:]
-	case 0x02:
-		// UTF-8 string
-		if len(rest) < 5 {
-			r.err = errors.New("corrupt BSON reading utf8 string len")
-			return false
-		}
-		var elen int
-		elen, rest = readInt32(rest)
-		if len(rest) < elen {
-			r.err = errors.New("corrupt BSON reading utf8 string")
-			return false
-		}
-		element = rest[:elen]
-		rest = rest[elen:]
-	case 0x3:
-		// BSON document
-		fallthrough
-	case 0x04:
-		// array (as BSON document)
-		var elen int
-		elen, _ = readInt32(rest)
-		if len(rest) < elen {
-			r.err = fmt.Errorf("corrupt document: want %x bytes, have %x", elen, len(rest))
-			return false
-		}
-		element = rest[:elen]
-		rest = rest[elen:]
-	case 0x07:
-		// object id
-		if len(rest) < 12 {
-			r.err = errors.New("corrupt BSON reading object id")
-			return false
-		}
-		element, rest = rest[:12], rest[12:]
-	case 0x08:
-		// boolean
-		if len(rest) < 1 {
-			r.err = errors.New("corrupt BSON reading boolean")
-			return false
-		}
-		element, rest = rest[:1], rest[1:]
-	case 0x09:
-		// UTC datetime
-		// int64
-		if len(rest) < 8 {
-			r.err = errors.New("corrupt BSON reading utc datetime")
-			return false
-		}
-		element, rest = rest[:8], rest[8:]
-	case 0x0a:
-		// null
-		element, rest = rest[:0], rest[0:]
-	case 0x0b:
-		// regex
-		if len(rest) < 2 {
-			// need at least two bytes for empty cstrings
-			r.err = errors.New("corrupt BSON reading regex")
-			return false
-		}
-		i := bytes.IndexByte(rest, 0)
-		if i < 0 {
-			r.err = errors.New("corrupt BSON regex 1")
-			return false
-		}
-		i++
-		j := bytes.IndexByte(rest[i+1:], 0)
-		if j < 0 {
-			r.err = errors.New("corrupt BSON regex 2")
-			return false
-		}
-		j++
-		element, rest = rest[:i+j+1], rest[i+j+1:]
-	case 0x10:
-		// int32
-		if len(rest) < 4 {
-			r.err = errors.New("corrupt BSON reading int32")
-			return false
-		}
-		element, rest = rest[:4], rest[4:]
-	case 0x11:
-		// timestamp
-		fallthrough
-	case 0x12:
-		// int64
-		if len(rest) < 8 {
-			r.err = errors.New("corrupt BSON reading int64")
-			return false
-		}
-		element, rest = rest[:8], rest[8:]
-	default:
-		r.err = fmt.Errorf("bson: unknown element type %x", typ)
-		return false
-	}
-	r.bson, r.ename, r.element = rest, ename, element
-	return true
-}
-
-// Err returns the first error that was encountered.
-func (r *reader) Err() error {
-	return r.err
-}
-
-// Element returns the most recent element read by a call to Next.
-func (r *reader) Element() (byte, []byte, []byte) {
-	return r.ename[0], r.ename[1:], r.element
-}
-
-// readInt32 returns the value of the first 4 bytes of buf as a little endian
-// int32. The remaining bytes are return as a convenience.
-// If there is less than 4 bytes of data in buf, the function will panic.
-func readInt32(buf []byte) (int, []byte) {
-	v := int(buf[0]) | int(buf[1])<<8 | int(buf[2])<<16 | int(buf[3])<<24
-	return v, buf[4:]
-}
-
-// readCstring returns a []byte representing the cstring value, including
-// the trailing \0.
-func readCstring(buf []byte) ([]byte, []byte, error) {
-	switch i := bytes.IndexByte(buf, 0); i {
-	case -1:
-		return nil, nil, errors.New("bson: cstring missing \\0")
-	default:
-		i++
-		return buf[:i], buf[i:], nil
-	}
-}
-
-func unmarshalMatch(data []byte) Match {
+func unmarshalMatch(data []byte) (string, interface{}) {
 	iter := reader{bson: data[4 : len(data)-1]}
 	m := Match{}
 	for iter.Next() {
@@ -207,72 +31,131 @@ func unmarshalMatch(data []byte) Match {
 			}
 		}
 	}
-	return m
+	return "", m
 }
 
-func unmarshalMatches(data []byte, d *ProfileDoc) error {
-	iter := reader{bson: data[4 : len(data)-1]}
-	for iter.Next() {
-		_, _, element := iter.Element()
-		d.Analysis.CgMlst.Matches = append(d.Analysis.CgMlst.Matches, unmarshalMatch(element))
+func unmarshalMatches(data []byte, p *Profile) error {
+	iter := bsonkit.Reader(data)
+	for {
+		element, err := iter.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		gene, id := unmarshalMatch(element.Bytes)
+		p.Matches[gene] = id
 	}
 	return iter.Err()
 }
 
-func unmarshalCgMlst(data []byte, d *ProfileDoc) error {
-	iter := reader{bson: data[4 : len(data)-1]}
-	for iter.Next() {
-		_, ename, element := iter.Element()
-		key := string(trimlast(ename))
-		switch key {
+func unmarshalCgMlst(data []byte, p *Profile) error {
+	iter := bsonkit.Reader(data)
+	for {
+		element, err := iter.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		switch element.Key() {
 		case "__v":
-			d.Analysis.CgMlst.Version = string(trimlast(element))
+			p.Version = element.Value()
 		case "matches":
-			unmarshalMatches(element, d)
+			unmarshalMatches(element.Bytes, p)
 		}
 	}
 	return iter.Err()
 }
 
-func unmarshalAnalysis(data []byte, d *ProfileDoc) error {
-	iter := reader{bson: data[4 : len(data)-1]}
-	for iter.Next() {
-		_, ename, element := iter.Element()
-		key := string(trimlast(ename))
-		switch key {
+func unmarshalAnalysis(data []byte, p *Profile) error {
+	iter := bsonkit.Reader(data)
+	for {
+		element, err := iter.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		switch element.Key() {
 		case "cgmlst":
-			unmarshalCgMlst(element, d)
+			unmarshalCgMlst(element, p)
 		}
 	}
 	return iter.Err()
 }
 
-func Unmarshal(data []byte, profile *ProfileDoc) error {
-	iter := reader{bson: data[4 : len(data)-1]}
-	for iter.Next() {
-		_, ename, element := iter.Element()
-		key := string(trimlast(ename))
+// func Unmarshal(data []byte, profile *ProfileDoc) error {
+// 	iter := reader{bson: data[4 : len(data)-1]}
+// 	for iter.Next() {
+// 		_, ename, element := iter.Element()
+// 		key := string(trimlast(ename))
 
-		switch key {
+// 		switch key {
+// 		case "_id":
+// 			var oid ObjectID
+// 			copy(oid[:], element)
+// 			profile.ID = oid
+// 		case "fileId":
+// 			profile.FileID = string(trimlast(element))
+// 		case "organismId":
+// 			profile.OrganismID = string(trimlast(element))
+// 		case "public":
+// 			profile.Public = element[0] == 1
+// 		case "analysis":
+// 			unmarshalAnalysis(element, profile)
+// 		}
+// 	}
+// 	return iter.Err()
+// }
+
+func parseProfile(data []byte) Profile {
+	profile := Profile{}
+	iter := bsonkit.Reader(data)
+	for {
+		element, err := iter.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		switch element.Key() {
 		case "_id":
-			var oid ObjectID
-			copy(oid[:], element)
-			profile.ID = oid
+			profile.ID = element.Value()
 		case "fileId":
-			profile.FileID = string(trimlast(element))
+			profile.FileID = element.Value()
 		case "organismId":
-			profile.OrganismID = string(trimlast(element))
+			profile.OrganismID = element.Value()
 		case "public":
-			profile.Public = element[0] == 1
+			profile.Public = element.Value()
 		case "analysis":
-			unmarshalAnalysis(element, profile)
+			unmarshalAnalysis(element.Bytes, profile)
 		}
 	}
-	return iter.Err()
+	return profile
 }
 
-func makeProfile(data []byte) (*ProfileDoc, error) {
-	profile := ProfileDoc{}
-	err := Unmarshal(data, &profile)
-	return &profile, err
+func parseGenomes(data []byte) GenomesDoc {
+
+}
+
+func parse(r io.Reader) {
+	for iter := bsonkit.GetDocuments(r); iter.Next(); {
+		if bytes.Contains(iter.bytes, []byte("genomes")) {
+			genomes := parseGenomes(iter.bytes)
+			// do something with the genomes
+		}
+		if bytes.Contains(iter.bytes, []byte("cgmlst")) {
+			profile := parseProfile(iter.bytes)
+			// do something with the profile
+		}
+		if bytes.Contains(iter.bytes, []byte("scores")) {
+			scores := parseScores(iter.bytes)
+			// do something with the scores
+		}
+	}
 }
