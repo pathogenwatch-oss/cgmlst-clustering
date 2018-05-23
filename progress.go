2ea7cca5b42c6ce026e2a71ac61617d8fc8c52c5
Ben Taylor <ben.taylor@sanger.ac.uk>
2018-05-23 16:39:51 +0100

    Fix progress tracking

--- progress.go
+++ progress.go
@@ -8,9 +8,18 @@ func UpdateProgress(expectedEvents int, events chan bool) (p chan ProgressMessag
 	p = make(chan ProgressMessage, 1000)
 	var eventCount int
 	updateStep := expectedEvents / 400
+	if updateStep == 0 {
+		updateStep = 1
+	}
 	nextUpdate := updateStep
 	go func() {
+		if expectedEvents == 0 {
+			p <- ProgressMessage{99.99}
+		}
 		for range events {
+			if expectedEvents == 0 {
+				continue
+			}
 			eventCount++
 			if eventCount > nextUpdate {
 				perc := 100.0 * float32(eventCount) / float32(expectedEvents)
