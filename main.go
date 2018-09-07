3de3dd3fa652a9373e3697f587b383edc08d99b7
Ben Taylor <ben.taylor@sanger.ac.uk>
2018-09-07 11:14:06 +0100

    Integrate new input and outputs

--- main.go
+++ main.go
@@ -11,22 +11,6 @@ import (
 	"gitlab.com/cgps/bsonkit"
 )
 
-type ClusterDetails struct {
-	Genome  bsonkit.ObjectID `json:"genome"`
-	Cluster bsonkit.ObjectID `json:"cluster"`
-}
-
-type ClusterOutput struct {
-	Threshold int              `json:"threshold"`
-	Genomes   []ClusterDetails `json:"genomes"`
-}
-
-type ClusterIndex struct {
-	Pi     []int    `json:"pi"`
-	Lambda []int    `json:"lambda"`
-	Sts    []string `json:"sts"`
-}
-
 func isSmaller(a, b bsonkit.ObjectID) bool {
 	return bytes.Compare(a[:], b[:]) < 0
 }
@@ -37,34 +21,19 @@ func main() {
 	progress := ProgressWorker(enc)
 	defer func() { progress <- ProgressEvent{EXIT, 0} }()
 
-	STs, _, profiles, scores, _, err := parse(r, progress)
+	STs, profiles, scores, maxThreshold, existingClusters, canReuseCache, err := parse(r, progress)
 	if err != nil {
 		panic(err)
 	}
 
-	cacheDocs := make(chan CacheOutput, 1000)
-	cacheFinished := make(chan bool, 2)
-	go func() {
-		for {
-			doc, more := <-cacheDocs
-			if !more {
-				cacheFinished <- true
-				return
-			}
-			enc.Encode(doc)
-			progress <- ProgressEvent{CACHED_RESULT, 1}
-		}
-	}()
-
-	scoreCache := MakeScoreCacher(&scores, cacheDocs)
 	tick := time.Tick(time.Second)
 	go func() {
 		for {
 			<-tick
-			log.Printf("%d scores remaining\n", scoreCache.Remaining())
+			log.Printf("%d scores remaining\n", scores.Todo())
 		}
 	}()
-	scoreComplete, errChan := scoreAll(scores, profiles, progress, scoreCache)
+	scoreComplete, errChan := scoreAll(scores, profiles, progress)
 
 	select {
 	case err := <-errChan:
@@ -82,19 +51,21 @@ func main() {
 	progress <- ProgressEvent{DISTANCES_COMPLETE, 0}
 
 	progress <- ProgressEvent{CLUSTERING_STARTED, 0}
-	clusters, err := NewClusters(len(scores.STs), distances)
-	if err != nil {
-		panic(err)
+	var clusters Clusters
+	if canReuseCache {
+		clusters, err = UpdateClusters(existingClusters, len(scores.STs), distances)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		clusters, err = NewClusters(len(scores.STs), distances)
+		if err != nil {
+			panic(err)
+		}
 	}
 
-	enc.Encode(ClusterIndex{
-		clusters.pi,
-		clusters.lambda,
-		scores.STs,
-	})
+	enc.Encode(clusters.Format(maxThreshold, distances, scores.STs))
 
-	<-cacheFinished
-
-	log.Printf("%d scores remaining\n", scoreCache.Remaining())
+	log.Printf("%d scores remaining\n", scores.Todo())
 	log.Printf("STs: %d; Scores: %d\n", len(STs), len(scores.scores))
 }
