a979738a3a8ea7f0ffab3d4e159f6853ada994f8
Ben Taylor <ben.taylor@sanger.ac.uk>
2018-09-18 17:49:26 +0100

    Add tests for binned genomes

---
+++ main_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestSubset(t *testing.T) {
+	testFile, err := os.Open("testdata/TestRequestIsSubset.bson")
+	if err != nil {
+		t.Fatal("Couldn't load test data")
+	}
+	progress := ProgressSinkHole()
+	STs, profiles, scores, maxThreshold, _, canReuseCache, err := parse(testFile, progress)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(STs, []CgmlstSt{"a", "b"}) {
+		t.Fatal("STs")
+	}
+	if maxThreshold != 4 {
+		t.Fatal("maxThreshold")
+	}
+	if canReuseCache {
+		t.Fatal("Cannot reuse cache")
+	}
+
+	distances, err := scores.Distances()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(distances, []int{1}) {
+		t.Fatal(distances)
+	}
+
+	scoreComplete, errChan := scoreAll(&scores, &profiles, progress)
+
+	select {
+	case err := <-errChan:
+		if err != nil {
+			t.Fatal(err)
+		}
+	case <-scoreComplete:
+	}
+
+	distances, err = scores.Distances()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(distances, []int{1}) {
+		t.Fatal(distances)
+	}
+
+	clusters, err := NewClusters(len(scores.STs), distances)
+	if err != nil {
+		t.Fatal(err)
+	}
+	output := clusters.Format(maxThreshold, distances, scores.STs)
+
+	for dist := 0; dist <= 4; dist++ {
+		doc := <-output
+		if _, found := doc.Edges[dist]; !found {
+			t.Fatal(dist)
+		}
+	}
+	doc := <-output
+	if len(doc.Sts) != 2 {
+		t.Fatal("Expected 2 STs")
+	}
+}
